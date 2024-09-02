ab8f55e194f8f7a025092a200fbfc8e811b8a9d9
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-09-02 14:18:28 +0900

    表示させるユーザー一覧取得機能と、検索機能実装

--- backend/src/controller/user.ts
+++ backend/src/controller/user.ts
@@ -3,6 +3,7 @@ import ErrorHandler from "../utils/utility-class";
 import { User } from "../models/user";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import { TryCatch } from "../middleware/error";
 
 const saltRounds = 10;
 
@@ -73,7 +74,7 @@ export const login = async (
       throw new ErrorHandler("サーバー設定エラー", 500);
     }
 
-    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET!, {
       expiresIn: "7d",
     });
 
@@ -91,3 +92,53 @@ export const login = async (
     next(error);
   }
 };
+
+export const getAllUsers = TryCatch(async (req: Request, res: Response) => {
+  const search = (req.query.search as string) || "";
+  const page = Number(req.query.page || 1);
+  const limit = Number(req.query.limit || 5);
+  const searchRegExp = new RegExp(search, "i");
+
+  const filter = {
+    $or: [
+      { name: { $regex: searchRegExp } },
+      { email: { $regex: searchRegExp } },
+      { phone: { $regex: searchRegExp } },
+    ],
+  };
+
+  const options = {
+    password: 0,
+    __v: 0,
+  };
+
+  const skip = (page - 1) * limit;
+
+  const [users, totalCount] = await Promise.all([
+    User.find(filter, options).skip(skip).limit(limit),
+    User.countDocuments(filter),
+  ]);
+
+  if (users.length === 0) {
+    return res
+      .status(404)
+      .json({ message: "条件に一致するユーザーが見つかりません" });
+  }
+
+  const totalPages = Math.ceil(totalCount / limit);
+  const previousPage = page > 1 ? page - 1 : null;
+  const nextPage = page < totalPages ? page + 1 : null;
+
+  return res.status(200).json({
+    message: "Success",
+    users,
+    pagination: {
+      currentPage: page,
+      previousPage: previousPage,
+      nextPage: nextPage,
+      totalPages: totalPages,
+      totalUsers: totalCount,
+      usersPerPage: limit,
+    },
+  });
+});
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,6 +1,7 @@
 import express from "express";
-import { login, register } from "../controller/user";
+import { getAllUsers, login, register } from "../controller/user";
 
 export const userRoutes = express.Router();
 userRoutes.post("/register", register);
 userRoutes.post("/login", login);
+userRoutes.get("/all", getAllUsers);
