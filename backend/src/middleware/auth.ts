d728ed58708329a5c881642ff28cd8caf63b976a
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-09-03 18:22:33 +0900

    POSTMANで各メソッドの動作確認

---
+++ backend/src/middleware/auth.ts
@@ -0,0 +1,47 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import ErrorHandler from "../utils/utility-class";
+import { User } from "../models/user";
+
+interface AuthenticatedRequest extends Request {
+  user?: any;
+}
+
+export const requireSignIN = (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const decoded = jwt.verify(
+      req.headers.authorization as string,
+      process.env.JWT_SECRET as string
+    );
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return next(new ErrorHandler("認証エラー", 401));
+  }
+};
+
+export const isAdmin = async (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = await User.findById(req.user?._id);
+    if (!user) {
+      return next(new ErrorHandler("ユーザーが見つかりません", 404));
+    }
+    if (user.role !== "admin") {
+      return next(
+        new ErrorHandler("認証エラーです。管理者権限が必要です", 401)
+      );
+    }
+    next(); // ユーザーが管理者の場合、次のミドルウェアに進む
+  } catch (error) {
+    console.error(error);
+    return next(new ErrorHandler("サーバーエラーが発生しました", 500));
+  }
+};
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import { getAllUsers, login, register } from "../controller/user";
+import { isAdmin, requireSignIN } from "../middleware/auth";
 
 export const userRoutes = express.Router();
 userRoutes.post("/register", register);
 userRoutes.post("/login", login);
-userRoutes.get("/all", getAllUsers);
+userRoutes.get("/test-all", getAllUsers);
+userRoutes.get("/all", requireSignIN, isAdmin, getAllUsers);
